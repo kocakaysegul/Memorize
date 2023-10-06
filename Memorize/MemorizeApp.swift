47c99594f9f628b15797ca40db4e92238fa0d760
kocakaysegul <aysegulkocak112@gmail.com>
2023-10-06 12:59:23 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Ayşegül Koçak on 6.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
